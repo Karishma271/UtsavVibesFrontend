b948befed5686fadedf437f3bc500203aab9800f
Karishma271 <karishmapatel273@gmail.com>
2024-12-14 01:14:51 -0500

    add

--- src/components/pages/venue/VenueList.jsx
+++ src/components/pages/venue/VenueList.jsx
@@ -40,7 +40,18 @@ const VenueList = () => {
         {venues.length > 0 ? (
           venues.map((venue) => (
             <div className="fpItem" key={venue._id}>
-             
+             <Link to={`/venue/${venue._id}`}>
+                {/* Use imageUrl */}
+                {venue.imageUrl ? (
+                  <img
+                    src={venue.imageUrl} // Directly use Google-hosted URL
+                    alt={venue.venueName}
+                    className="venueImage" // Add class for styling if needed
+                  />
+                ) : (
+                  <div className="noImage">No Image Available</div> // Fallback
+                )}
+              </Link>
               <span className="fpName">{venue.venueName}</span>
               <span className="fpCity">
                 <LocationOnIcon /> {venue.address}
