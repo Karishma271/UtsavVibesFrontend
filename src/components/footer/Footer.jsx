042aa86c2add08f73d145c7148b6ee24156958da
Karishma271 <karishmapatel273@gmail.com>
2024-12-14 02:27:11 -0500

    add

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -5,42 +5,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import "./footer.css"
 
-const MainContent = styled.div`
-  min-height: calc(100vh - /* height of footer */);
-  display: flex;
-  flex-direction: column;
-  padding-top:20px /* height of footer */;
-`;
-
-const FooterContainer = styled.footer`
-  background-color: #333333;
-  color: white;
-  padding: 20px;
-  width: 100%;
-`;
-
-const Container = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-`;
-
-const FooterText = styled.div`
-  font-family: 'DM Sans', sans-serif;
-`;
-
-const SocialMediaLinks = styled.div`
-  a {
-    text-decoration: none;
-    color: white;
-    margin-right: 20px;
-    font-size: 24px;
-  }
-  a:hover {
-    color: #7848f4;
-  }
-`;
-
 const Footer = () => {
   return (
     <MainContent>
@@ -50,17 +14,17 @@ const Footer = () => {
         <Container>
           <FooterText>&copy; 2024 UtsavVibes</FooterText>
 
-          <SocialMediaLinks>
+          <SocialMediaLinks role="navigation">
             <a href="https://www.facebook.com" aria-label="Visit our Facebook page" >
               <i className="fab fa-facebook"></i>
             </a>
-            <a href="https://www.twitter.com" aria-label="Visit our twitter page">
+            <a href="https://www.twitter.com" aria-label="Visit our Twitter page">
               <i className="fab fa-twitter"></i>
             </a>
-            <a href="https://www.linkedin.com" aria-label="Visit our linkedin page">
+            <a href="https://www.linkedin.com" aria-label="Visit our LinkedIn page">
               <i className="fab fa-linkedin"></i>
             </a>
-            <a href="https://www.instagram.com" aria-label="Visit our instagram page">
+            <a href="https://www.instagram.com" aria-label="Visit our Instagram page">
               <i className="fab fa-instagram"></i>
             </a>
           </SocialMediaLinks>
