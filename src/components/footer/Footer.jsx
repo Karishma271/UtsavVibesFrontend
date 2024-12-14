180d6c09c62dbf0187d7184f2154329e65764948
Karishma271 <karishmapatel273@gmail.com>
2024-12-14 02:30:08 -0500

    add

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,32 +3,60 @@ import styled from 'styled-components';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
-import "./footer.css"
+import "./footer.css";
+
+const MainContent = styled.div`
+  min-height: calc(100vh - 80px); /* Adjust based on footer height */
+  display: flex;
+  flex-direction: column;
+  padding-top: 20px; /* Ensure space above the footer */
+`;
+
+const FooterContainer = styled.footer`
+  background-color: #333333;
+  color: white;
+  padding: 20px 0;
+  width: 100%;
+`;
+
+const FooterTextSection = styled.div`
+  text-align: center;
+  font-family: 'DM Sans', sans-serif;
+  font-size: 14px;
+`;
+
+const SocialMediaLinksSection = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 10px;
+`;
 
 const Footer = () => {
   return (
     <MainContent>
-      {/* Your other components/content go here */}
+      {/* Other content of your page goes here */}
 
       <FooterContainer>
-        <Container>
-          <FooterText>&copy; 2024 UtsavVibes</FooterText>
+        {/* Footer Text */}
+        <FooterTextSection>
+          <p>&copy; 2024 UtsavVibes</p>
+        </FooterTextSection>
 
-          <SocialMediaLinks role="navigation">
-            <a href="https://www.facebook.com" aria-label="Visit our Facebook page" >
-              <i className="fab fa-facebook"></i>
-            </a>
-            <a href="https://www.twitter.com" aria-label="Visit our Twitter page">
-              <i className="fab fa-twitter"></i>
-            </a>
-            <a href="https://www.linkedin.com" aria-label="Visit our LinkedIn page">
-              <i className="fab fa-linkedin"></i>
-            </a>
-            <a href="https://www.instagram.com" aria-label="Visit our Instagram page">
-              <i className="fab fa-instagram"></i>
-            </a>
-          </SocialMediaLinks>
-        </Container>
+        {/* Social Media Links */}
+        <SocialMediaLinksSection>
+          <a href="https://www.facebook.com" aria-label="Visit our Facebook page">
+            <i className="fab fa-facebook"></i>
+          </a>
+          <a href="https://www.twitter.com" aria-label="Visit our Twitter page">
+            <i className="fab fa-twitter"></i>
+          </a>
+          <a href="https://www.linkedin.com" aria-label="Visit our LinkedIn page">
+            <i className="fab fa-linkedin"></i>
+          </a>
+          <a href="https://www.instagram.com" aria-label="Visit our Instagram page">
+            <i className="fab fa-instagram"></i>
+          </a>
+        </SocialMediaLinksSection>
       </FooterContainer>
     </MainContent>
   );
