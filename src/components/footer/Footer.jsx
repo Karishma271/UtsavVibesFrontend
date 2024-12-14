bb0096a295ddcdf81b98404c27ec64766fcf1b20
Karishma271 <karishmapatel273@gmail.com>
2024-12-14 02:34:26 -0500

    add

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,39 +1,47 @@
 import React from 'react';
-import styled from 'styled-components';
-import '@fortawesome/fontawesome-free/css/all.min.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
-import "./footer.css";
-
+import { Link } from 'react-router-dom';
+import { Typography, Button } from '@mui/material';
+import { Facebook, Twitter, LinkedIn, Instagram } from '@mui/icons-material';
+import './footer.css';
 
 const Footer = () => {
   return (
-    <MainContent>
-      {/* Other content of your page goes here */}
-
-      <FooterContainer>
+    <footer className="footer">
+      <div className="footer-container">
         {/* Footer Text */}
-        <FooterTextSection>
-          <p>&copy; 2024 UtsavVibes</p>
-        </FooterTextSection>
+        <div className="footer-text">
+          <Typography variant="h6" component="p" align="center">
+            &copy; 2024 UtsavVibes. All rights reserved.
+          </Typography>
+        </div>
 
         {/* Social Media Links */}
-        <SocialMediaLinksSection>
-          <a href="https://www.facebook.com" aria-label="Visit our Facebook page">
-            <i className="fab fa-facebook"></i>
-          </a>
-          <a href="https://www.twitter.com" aria-label="Visit our Twitter page">
-            <i className="fab fa-twitter"></i>
-          </a>
-          <a href="https://www.linkedin.com" aria-label="Visit our LinkedIn page">
-            <i className="fab fa-linkedin"></i>
-          </a>
-          <a href="https://www.instagram.com" aria-label="Visit our Instagram page">
-            <i className="fab fa-instagram"></i>
-          </a>
-        </SocialMediaLinksSection>
-      </FooterContainer>
-    </MainContent>
+        <div className="footer-social">
+          <Link to="https://www.facebook.com" aria-label="Visit our Facebook page">
+            <Facebook />
+          </Link>
+          <Link to="https://www.twitter.com" aria-label="Visit our Twitter page">
+            <Twitter />
+          </Link>
+          <Link to="https://www.linkedin.com" aria-label="Visit our LinkedIn page">
+            <LinkedIn />
+          </Link>
+          <Link to="https://www.instagram.com" aria-label="Visit our Instagram page">
+            <Instagram />
+          </Link>
+        </div>
+
+        {/* Footer Buttons */}
+        <div className="footer-buttons">
+          <Button variant="outlined" color="primary" className="footer-btn">
+            Privacy Policy
+          </Button>
+          <Button variant="outlined" color="primary" className="footer-btn">
+            Terms of Service
+          </Button>
+        </div>
+      </div>
+    </footer>
   );
 };
 
