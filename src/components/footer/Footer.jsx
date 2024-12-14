06b5b2d4fe9ec4e3bda1b07ee2e1edc7ea146f19
Karishma271 <karishmapatel273@gmail.com>
2024-12-14 02:31:48 -0500

    add

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -5,31 +5,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import "./footer.css";
 
-const MainContent = styled.div`
-  min-height: calc(100vh - 80px); /* Adjust based on footer height */
-  display: flex;
-  flex-direction: column;
-  padding-top: 20px; /* Ensure space above the footer */
-`;
-
-const FooterContainer = styled.footer`
-  background-color: #333333;
-  color: white;
-  padding: 20px 0;
-  width: 100%;
-`;
-
-const FooterTextSection = styled.div`
-  text-align: center;
-  font-family: 'DM Sans', sans-serif;
-  font-size: 14px;
-`;
-
-const SocialMediaLinksSection = styled.div`
-  display: flex;
-  justify-content: center;
-  margin-top: 10px;
-`;
 
 const Footer = () => {
   return (
